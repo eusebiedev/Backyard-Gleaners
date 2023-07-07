d5a7b79871c74a0f5ae6de19ef72f159e24d8d71
Eusebie Siebenberg <siebenee@gmail.com>
2023-07-07 15:57:17 -0700

    update capstone proposal, readme, ApiContext files

--- BackyardGleanersAPI/Models/BackyardGleanersApiContext.cs
+++ BackyardGleanersAPI/Models/BackyardGleanersApiContext.cs
@@ -12,16 +12,17 @@ namespace BackyardGleanersApi.Models
     {
       builder.Entity<Host>()
         .HasData(
-          new Host { },
-          new Host { },
-          new Host { },
-          new Host { },
-          new Host { },
-          new Host { },
-          new Host { },
-          new Host { },
-          new Host { },
-          new Host { }
+          new Host { HostId = 1, },
+          new Host { HostId = 2, },
+          new Host { HostId = 3, },
+          new Host { HostId = 4, },
+          new Host { HostId = 5, },
+          new Host { HostId = 6, },
+          new Host { HostId = 7, },
+          new Host { HostId = 8, },
+          new Host { HostId = 9, },
+          new Host { HostId = 10, }
+
         );
     }
   }
