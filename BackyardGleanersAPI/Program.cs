6cf15e283203a92f432caaee5897816cf3028d9b
Eusebie Siebenberg <siebenee@gmail.com>
2023-07-07 14:22:34 -0700

    Add base project folder structure, necessary build files, capstone proposal

---
+++ BackyardGleanersAPI/Program.cs
@@ -0,0 +1,62 @@
+using BackyardGleanersApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BackyardGleanersApiContext>(
+                dbContextOptions => dbContextOptions
+                  .UseMySql(
+                  builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                  ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                  )
+                )
+              );
+
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                      opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                                      opt.AssumeDefaultVersionWhenUnspecified = true;
+                                      opt.ReportApiVersions = true;
+                                      opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                      new HeaderApiVersionReader("x-api-version"),
+                                                                                      new MediaTypeApiVersionReader("x-api-version"));
+                                    });
+
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+  setup.GroupNameFormat = "'v'VVV";
+  setup.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+var app = builder.Build();
+
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI(options =>
+  {
+    foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+    {
+      options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+      description.GroupName.ToUpperInvariant());
+    }
+  });
+}
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ capstone_proposal.md
@@ -0,0 +1,29 @@
+# Eusebie Siebenberg
+
+## Name of Project:
+* Backyard Gleaners
+
+## Project's Purpose or Goal: (What will it do for users?)
+* Develop and deploy a website for Hosts to connect with Gleaners to harvest and/or distribute surplus foods.
+
+## List the absolute minimum features the project requires to meet this purpose or goal:
+* MVC Client Front End: Home, About, History, Faq, Benefits, Profiles
+* Database of Hosts from custom API
+* Gleaner Account Creation to connect with Hosts
+
+## What tools, frameworks, libraries, APIs, modules and/or other resources (whatever is specific to your track, and your language) will you use to create this MVP? List them all here. Be specific.
+* C#
+* MySQL
+* Custom API
+* MVC Client Front End
+* .NET, ASP.NET Core
+* EF Core Migrations
+
+## If you finish developing the minimum viable product (MVP) with time to spare, what will you work on next? Describe these features here: Be specific.
+* 
+
+## What additional tools, frameworks, libraries, APIs, or other resources will these additional features require?
+* 
+
+## Is there anything else you'd like your instructor to know?
+* 
