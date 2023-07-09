6a5e1dd84c82d7f14f872772875053b03b84d06c
Eusebie Siebenberg <siebenee@gmail.com>
2023-07-08 18:58:02 -0700

    rename dbset, dbcontext to account for new naming convention

--- BackyardGleanersApi/Models/BackyardGleanersApiContext.cs
+++ BackyardGleanersApi/Models/BackyardGleanersApiContext.cs
@@ -4,25 +4,9 @@ namespace BackyardGleanersApi.Models
 {
   public class BackyardGleanersApiContext : DbContext
   {
-    public DbSet<Host> Hosts { get; set; }
+    public DbSet<Gardener> Gardeners { get; set; }
     public BackyardGleanersApiContext(DbContextOptions<BackyardGleanersApiContext> options) : base(options)
     {
-    }
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-      builder.Entity<Host>()
-        .HasData(
-          new Host { HostId = 1, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 2, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 3, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 4, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 5, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 6, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 7, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 8, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 9, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" },
-          new Host { HostId = 10, Name = "", Description = "", Food = "", Availability = "", Location = "", Contact = "" }
-        );
     }
   }
 }
