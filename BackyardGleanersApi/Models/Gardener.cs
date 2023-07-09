85448a2bbe6eb97f483b6fc9dbc2f6c2939ed94c
Eusebie Siebenberg <siebenee@gmail.com>
2023-07-08 18:59:02 -0700

    Add new Gardener.cs model to resolve ambiguous naming error

---
+++ BackyardGleanersApi/Models/Gardener.cs
@@ -0,0 +1,14 @@
+namespace BackyardGleanersApi.Models
+{
+  public class Gardener 
+  {
+    public int GardenerId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; } 
+    public string Food { get; set; }
+    public string Availability {get; set; }
+    public string Location { get; set; }
+    public string Contact { get; set; }
+    public string ProfilePic { get; set; } 
+  }
+}
