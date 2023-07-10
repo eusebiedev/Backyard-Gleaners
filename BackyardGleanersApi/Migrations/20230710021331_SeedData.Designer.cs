5766d1e2e5244debb7a9cab11858047ae2dd29c5
Eusebie Siebenberg <siebenee@gmail.com>
2023-07-09 19:25:58 -0700

    Add SeedData migrations, update database, test Postman with success

--- BackyardGleanersApi/Migrations/20230710021331_SeedData.Designer.cs
+++ BackyardGleanersApi/Migrations/20230710021331_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using BackyardGleanersApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackyardGleanersApi.Migrations
 {
     [DbContext(typeof(BackyardGleanersApiContext))]
-    partial class BackyardGleanersApiContextModelSnapshot : ModelSnapshot
+    [Migration("20230710021331_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
