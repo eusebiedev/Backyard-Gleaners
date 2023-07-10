d32456175c40c322ff2a36be2157abd986b56f4a
Eusebie Siebenberg <siebenee@gmail.com>
2023-07-09 19:56:46 -0700

    build apihelper.cs model for client

---
+++ BackyardGleanersClient/Models/ApiHelper.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace BackyardGleanersClient.Models
+{
+  public class ApiHelper
+  {
+    public static async Task<string> GetAll()
+    {
+      RestClient client = new RestClient("https://localhost:5001/");
+      RestRequest request = new RestRequest($"api/v2/gardeners", Method.Get);
+      RestResponse response = await client.GetAsync(request);
+      return response.Content;
+    }
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("https://localhost:5001/");
+      RestRequest request = new RestRequest($"api/v2/gardeners/{id}", Method.Get);
+      RestResponse response = await client.GetAsync(request);
+      return response.Content;
+    }
+    public static async void Post(string newGardener)
+    {
+      RestClient client = new RestClient("https://localhost:5001/");
+      RestRequest request = new RestRequest($"api/v2/gardeners", Method.Post);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newGardener);
+      await client.PostAsync(request);
+    }
+
+    public static async void Put(int id, string newGardener)
+    {
+      RestClient client = new RestClient("https://localhost:5001/");
+      RestRequest request = new RestRequest($"api/v2/gardeners/{id}", Method.Put);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newGardener);
+      await client.PutAsync(request);
+    }
+    public static async void Delete(int id)
+    {
+      RestClient client = new RestClient("https://localhost:5001/");
+      RestRequest request = new RestRequest($"api/v2/gardeners/{id}", Method.Delete);
+      request.AddHeader("Content-Type", "application/json");
+      await client.DeleteAsync(request);
+    }
+  }
+}
