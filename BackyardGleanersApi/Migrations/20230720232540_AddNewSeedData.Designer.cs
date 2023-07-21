87d20fd422066a75db912d3d666b20bfeadeaaa3
Eusebie Siebenberg <siebenee@gmail.com>
2023-07-20 21:23:08 -0700

    Add new seed data, update database

--- BackyardGleanersApi/Migrations/20230720232540_AddNewSeedData.Designer.cs
+++ BackyardGleanersApi/Migrations/20230720232540_AddNewSeedData.Designer.cs
@@ -2,6 +2,7 @@
 using BackyardGleanersApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackyardGleanersApi.Migrations
 {
     [DbContext(typeof(BackyardGleanersApiContext))]
-    partial class BackyardGleanersApiContextModelSnapshot : ModelSnapshot
+    [Migration("20230720232540_AddNewSeedData")]
+    partial class AddNewSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BackyardGleanersApi/Migrations/20230720232540_AddNewSeedData.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackyardGleanersApi.Migrations
+{
+    public partial class AddNewSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 1,
+                column: "Location",
+                value: "N Washburne Ave & N Lombard St, Portland, OR 97217");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 4,
+                column: "Location",
+                value: "SE 9th Ave & SE Division St, Portland, OR 97214");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 5,
+                column: "Location",
+                value: "NE 33rd Ave & NE Killingsworth St, Portland, OR 97211");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 6,
+                column: "Location",
+                value: "SW Condor Ave & SW Barbur Blvd, Portland, OR 97239");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 7,
+                column: "Location",
+                value: "NE 24th Ave & NE Broadway, Portland, OR 97232");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 8,
+                column: "Location",
+                value: "NW Skyline Boulevard & NW Cornell Road, Forest Park, Portland, OR 97229");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 9,
+                column: "Location",
+                value: "N Mason Street & N Castle Avenue, Portland, OR 97227, Overlook");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 10,
+                column: "Location",
+                value: "N Morgan Street & N Moore Avenue, Portland, OR 97217");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 11,
+                column: "Location",
+                value: "SE Duke St & SE 66th Ave, Portland, OR 97206");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 12,
+                column: "Location",
+                value: "NE Wygant Street & NE 10th Avenue, Portland, OR 97211");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 1,
+                column: "Location",
+                value: "N Washburne Ave & N Lombard St., Portland, OR 97217");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 4,
+                column: "Location",
+                value: "SE 9th Ave & SE Division StPortland, OR 97214");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 5,
+                column: "Location",
+                value: "NE 33rd Ave & NE Killingsworth StPortland, OR 97211");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 6,
+                column: "Location",
+                value: "SW Condor Ave & SW Barbur BlvdPortland, OR 97239");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 7,
+                column: "Location",
+                value: "NE 24th Ave & Northeast Broadway, Portland, OR 97232");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 8,
+                column: "Location",
+                value: "Northwest Skyline Boulevard & Northwest Cornell Road, Forest Park, Portland, OR 97229");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 9,
+                column: "Location",
+                value: "North Mason Street & North Castle Avenue, Portland, OR 97227");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 10,
+                column: "Location",
+                value: "North Morgan Street & North Moore Avenue, Portland, OR 97217");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 11,
+                column: "Location",
+                value: "SE Duke St & SE 66th AvePortland, OR 97206");
+
+            migrationBuilder.UpdateData(
+                table: "Gardeners",
+                keyColumn: "GardenerId",
+                keyValue: 12,
+                column: "Location",
+                value: "Northeast Wygant Street & Northeast 10th Avenue, Portland, OR 97211");
+        }
+    }
+}
