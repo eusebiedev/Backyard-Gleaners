e23f3bbc1c07f2bdd10663a0c28c3d3146ef166b
Eusebie Siebenberg <siebenee@gmail.com>
2023-08-10 12:02:42 -0700

    Api in development. Migrating from azure possibly

--- BackyardGleanersClient/Models/ApiHelper.cs
+++ BackyardGleanersClient/Models/ApiHelper.cs
@@ -7,7 +7,7 @@ public class ApiHelper : ControllerBase
 {
   public static async Task<string> GetAll()
   {
-    RestClient client = new RestClient("https://backyard-gleaners-app.azurewebsites.net");
+    RestClient client = new RestClient("https://localhost:5001");
     RestRequest request = new RestRequest($"api/v2/gardeners", Method.Get);
     RestResponse response = await client.GetAsync(request);
     return response.Content;
@@ -15,7 +15,7 @@ public class ApiHelper : ControllerBase
 //  https://backyard-gleaners-app.azurewebsites.net Azure API 
   public static async Task<string> Get(int id)
   {
-    RestClient client = new RestClient("https://backyard-gleaners-app.azurewebsites.net");
+    RestClient client = new RestClient("https://localhost:5001");
     RestRequest request = new RestRequest($"api/v2/gardeners/{id}", Method.Get);
     RestResponse response = await client.GetAsync(request);
     return response.Content;
@@ -23,7 +23,7 @@ public class ApiHelper : ControllerBase
 
   public static async void Post(string newGardener)
   {
-    RestClient client = new RestClient("https://backyard-gleaners-app.azurewebsites.net");
+    RestClient client = new RestClient("https://localhost:5001");
     RestRequest request = new RestRequest($"api/v2/gardeners", Method.Post);
     request.AddHeader("Content-Type", "application/json");
     request.AddJsonBody(newGardener);
@@ -32,7 +32,7 @@ public class ApiHelper : ControllerBase
 
   public static async void Put(int id, string newGardener)
   {
-    RestClient client = new RestClient("https://backyard-gleaners-app.azurewebsites.net");
+    RestClient client = new RestClient("https://localhost:5001");
     RestRequest request = new RestRequest($"api/v2/gardeners/{id}", Method.Put);
     request.AddHeader("Content-Type", "application/json");
     request.AddJsonBody(newGardener);
@@ -41,7 +41,7 @@ public class ApiHelper : ControllerBase
   
   public static async void Delete(int id)
   {
-    RestClient client = new RestClient("https://backyard-gleaners-app.azurewebsites.net");
+    RestClient client = new RestClient("https://localhost:5001");
     RestRequest request = new RestRequest($"api/v2/gardeners/{id}", Method.Delete);
     request.AddHeader("Content-Type", "application/json");
     await client.DeleteAsync(request);
@@ -50,7 +50,7 @@ public class ApiHelper : ControllerBase
   // custom api search method
   public static async Task<string> Search(string food, string location)
   {
-    RestClient client = new RestClient("https://backyard-gleaners-app.azurewebsites.net");
+    RestClient client = new RestClient("https://localhost:5001");
     RestRequest request = new RestRequest($"api/v2/gardeners/Search", Method.Get);
     request.AddParameter("food", food);
     request.AddParameter("location", location);
